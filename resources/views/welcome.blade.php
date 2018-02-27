7ec455f82316c142c4a7b54645d109a0bfac4e21
Kerry Jones <kerryjones21@gmail.com>
2018-02-27 00:19:57 -0800

    Added logo and started to create layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,50 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Splinter</title>
+    <link rel="icon" type="image/png" href="/images/splinter-logo.png">
     <link rel="stylesheet" href="{{ mix("css/bulma.css") }}">
     <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
-  </head>
-  <body>
-  <section class="section">
+</head>
+<body>
+<nav class="navbar is-dark">
     <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                <img src="/images/splinter-word-logo.png" alt="Splinter: a modern Crypto Backtesting Framework"
+                     width="112" height="28">
+            </a>
+            <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+
+        <div class="navbar-menu">
+
+            <div class="navbar-end">
+                <a class="navbar-item" href="/">
+                    Home
+                </a>
+                <a class="navbar-item" href="/documentation/overview/start/">
+                    Backtests
+                </a>
+            </div>
+        </div>
     </div>
-  </section>
-  </body>
+</nav>
+<section class="section">
+    <div class="container">
+        <h1 class="title">
+            Hello World
+        </h1>
+        <p class="subtitle">
+            My first website with <strong>Bulma</strong>!
+        </p>
+    </div>
+</section>
+</body>
 </html>
